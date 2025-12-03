41257c760f24554b7ab076c49285cc24b8470911
Tamas Kurics <tamas.kurics@gmail.com>
2025-12-03 09:02:47 +0100

    change test suite formatting
    Signed-off-by: Tamas Kurics <tamas.kurics@gmail.com>

--- src/test/scala/AoCSuite.scala
+++ src/test/scala/AoCSuite.scala
@@ -8,18 +8,18 @@ class AoCSuite extends AnyFreeSpec, Matchers {
 
     "Part 1" - {
       import Day01a.{Rotation, countZeroPositions, parseInput}
+
       "should count the number of times the dial is left pointing at 0 after any rotation in the sequence" in {
         val rotations: List[Rotation] = inputLines.map(parseInput)
-
         countZeroPositions(startPosition, rotations) shouldEqual 3
       }
     }
 
     "Part 2" - {
       import Day01b.{Rotation, countTotalZeroPositionPasses, parseInput}
+
       "should count the total number of times the dial passes 0 after any rotation in the sequence" in {
         val rotations: List[Rotation] = inputLines.map(parseInput)
-
         countTotalZeroPositionPasses(startPosition, rotations) shouldEqual 6
       }
     }
@@ -28,6 +28,7 @@ class AoCSuite extends AnyFreeSpec, Matchers {
   "Day02" - {
     "Part 1" - {
       import Day02a.{IdRange, totalInvalidIDSum}
+
       "should add all of the invalid IDs" - {
         val ranges: List[IdRange] = List(IdRange(11, 22), IdRange(95, 115), IdRange(1188511880L, 1188511890L))
         totalInvalidIDSum(ranges) shouldEqual 1188512017L
@@ -36,6 +37,7 @@ class AoCSuite extends AnyFreeSpec, Matchers {
 
     "Part 2" - {
       import Day02b.{IdRange, totalInvalidIDSum}
+
       "should add all of the invalid IDs" - {
         val ranges: List[IdRange] = List(IdRange(11, 22), IdRange(95, 115), IdRange(1188511880L, 1188511890L))
         totalInvalidIDSum(ranges) shouldEqual 1188512128L
@@ -53,6 +55,7 @@ class AoCSuite extends AnyFreeSpec, Matchers {
 
     "Part 1" - {
       import Day03a.calcSumOfJoltages
+
       "should calculate the total output joltage when 2 batteries are turned on" - {
         calcSumOfJoltages(packs) shouldEqual 357
       }
@@ -60,6 +63,7 @@ class AoCSuite extends AnyFreeSpec, Matchers {
 
     "Part 2" - {
       import Day03b.calcSumOfJoltages
+
       "should calculate the total output joltage when 12 batteries are turned on" - {
         calcSumOfJoltages(packs, 12) shouldEqual 3121910778619L
       }
